4f000ad0ba9f0bd7a286f92e6212835e6c2e6d81
taojian <1099593012@qq.com>
2018-07-18 21:07:57 +0800

    delete out

---
+++ src/com/javase/Test.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2018 Baidu, Inc. All Rights Reserved.
+ */
+package com.javase;
+
+/**
+ * @description:
+ * @author: taojian
+ * @create: 2018-07-15 19:01
+ **/
+public class Test {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public void fun(double c){
+        if (c==0.0){
+
+        }
+    }
+}
